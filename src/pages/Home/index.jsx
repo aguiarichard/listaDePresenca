9acaffb1507cfcbbc001a4cfb83adef086a77a9a
Richard de aguiar <richard_aguiar.19@hotmail.com>
2023-05-15 20:48:28 -0300

    criando componente Card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,49 @@
+import React, { useState } from "react"
+
 import "./styles.css"
 
+import { Card } from "../../components/Card"
+
 export function Home() {
+  const [studentName, setStudentName] = useState('')
+  const [students, setStudents] = useState([])
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    }
+
+    setStudents(prevStudent => [...prevStudent, newStudent])
+  }
+
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
-      <input type="text" placeholder="digite um nome..." />
-      <button>Adicionar</button>
+
+      <input
+        type="text"
+        placeholder="digite um nome..."
+        onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {
+        students.map(student => (
+          <Card
+            key={student.time}
+            name={student.name}
+            time={student.time}
+          />
+        ))
+      }
     </div>
   )
 }
