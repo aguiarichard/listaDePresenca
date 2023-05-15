1ab5e24aa73d20be01e310d6a12279e3a10c1c04
Richard de aguiar <richard_aguiar.19@hotmail.com>
2023-05-15 19:30:43 -0300

    adicionando projeto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="digite um nome..."/>
+      <button>Adicionar</button>
+    </div>
+  )
+}
